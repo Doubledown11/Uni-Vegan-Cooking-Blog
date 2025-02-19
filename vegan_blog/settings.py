57aa91b6698334c4f798b27dbf65565ebda9fae8
DoubleDown11 <115449065+Doubledown11@users.noreply.github.com>
2025-02-19 10:59:51 -0700

    Update settings.py

--- vegan_blog/settings.py
+++ vegan_blog/settings.py
@@ -161,7 +161,7 @@ PASSWORD_RESET_TIMEOUT = 259200
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_USE_TLS = True
-# DEFAULT_FROM_USER = 'dalice@ualberta.ca'
+# DEFAULT_FROM_USER = ''
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = 
